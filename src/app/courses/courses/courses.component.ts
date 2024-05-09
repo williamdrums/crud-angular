8eea9264e8bdb5c26c66b0ac90f4ecdcda3bd090
williamdrums <williandrums@gmail.com>
2024-05-08 22:42:55 -0400

    criando table de dados estaticos

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
+import { MatTableModule } from '@angular/material/table'
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit {
 
+courses: Course[] = [
+  { _id: '1', name: 'Angular', category: 'front-end'}
+];
+displayedColumns = ['name','category']
+
+  constructor(){
+    //this.courses = [];
+  }
+
+
+  ngOnInit(): void {
+
+  }
 }
